2d394ff7425e1bad27057cef9d5f8bb8e1536a9c
za233 <40100836+za233@users.noreply.github.com>
2022-03-16 20:32:57 +0800

    Update Utils.cpp

--- llvm/lib/Transforms/Obfuscation/Utils.cpp
+++ llvm/lib/Transforms/Obfuscation/Utils.cpp
@@ -57,3 +57,53 @@ BasicBlock* llvm::createCloneBasicBlock(BasicBlock *BB){
     }
     return cloneBB;
 }
+
+std::string llvm::readAnnotate(Function *f)   //取自原版ollvm项目
+{
+    std::string annotation = "";
+    /* Get annotation variable */
+    GlobalVariable *glob=f->getParent()->getGlobalVariable( "llvm.global.annotations" );
+    if ( glob != NULL )
+    {
+        /* Get the array */
+        if ( ConstantArray * ca = dyn_cast<ConstantArray>( glob->getInitializer() ) )
+        {
+            for ( unsigned i = 0; i < ca->getNumOperands(); ++i )
+            {
+                /* Get the struct */
+                if ( ConstantStruct * structAn = dyn_cast<ConstantStruct>( ca->getOperand( i ) ) )
+                {
+                    if ( ConstantExpr * expr = dyn_cast<ConstantExpr>( structAn->getOperand( 0 ) ) )
+                    {
+                        /*
+                         * If it's a bitcast we can check if the annotation is concerning
+                         * the current function
+                         */
+                        if ( expr->getOpcode() == Instruction::BitCast && expr->getOperand( 0 ) == f )
+                        {
+                            ConstantExpr *note = cast<ConstantExpr>( structAn->getOperand( 1 ) );
+                            /*
+                             * If it's a GetElementPtr, that means we found the variable
+                             * containing the annotations
+                             */
+                            if ( note->getOpcode() == Instruction::GetElementPtr )
+                            {
+                                if ( GlobalVariable * annoteStr = dyn_cast<GlobalVariable>( note->getOperand( 0 ) ) )
+                                {
+                                    if ( ConstantDataSequential * data = dyn_cast<ConstantDataSequential>( annoteStr->getInitializer() ) )
+                                    {
+                                        if ( data->isString() )
+                                        {
+                                            annotation += data->getAsString().lower() + " ";
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return(annotation);
+}
