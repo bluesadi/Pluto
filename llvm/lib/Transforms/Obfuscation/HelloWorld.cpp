2cc7c9155e7983ca94286b8898cea8c8f2440680
bluesadi <liuyibo@stu.scu.edu.cn>
2022-07-13 08:02:36 -0700

    Fix: #18

--- llvm/lib/Transforms/Obfuscation/HelloWorld.cpp
+++ llvm/lib/Transforms/Obfuscation/HelloWorld.cpp
@@ -9,15 +9,6 @@ using namespace llvm;
 
 bool HelloWorld::runOnFunction(Function &F){
     if(enable){
-        MatrixXd A(4, 5);
-        Vector4d b;
-        A << 0, 0, 0, 1, 1,
-             0, 1, 1, 1, 1,
-             1, 0, 1, 1, 1,
-             1, 1, 1, 0, 1;
-        b << 0, 0, 0, 0;
-        VectorXd x = A.fullPivLu().kernel().col(0);
-        std::cout << "DEBUG: " << x << "\n";
         outs() << "Hello, " << F.getName() << "\n";
     }
     return false;
