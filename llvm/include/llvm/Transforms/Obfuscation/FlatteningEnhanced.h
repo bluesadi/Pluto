757d11a15cdad163fa3939bebe0abbb34d906b7e
za233 <2509497833@qq.com>
2022-10-10 19:14:50 +0800

    FlatteningEnchaned pass remake

--- llvm/include/llvm/Transforms/Obfuscation/FlatteningEnhanced.h
+++ llvm/include/llvm/Transforms/Obfuscation/FlatteningEnhanced.h
@@ -3,43 +3,33 @@
 
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/BasicBlock.h"
 #include<list>
 #include<vector>
 
 namespace llvm
 {
-    struct TreeNode
-	{
-		unsigned int val=0,limit=0,l=0,r=0;
-		TreeNode *left=NULL,*right=NULL;
-	};
-    class MyFlatten : public FunctionPass
+    class MyFlatten : public ModulePass
     {
         public:
             static char ID;
             bool enable;
 
-            MyFlatten(bool enable) : FunctionPass(ID)
+            MyFlatten(bool enable) : ModulePass(ID)
             {
                 this->enable = enable;
             }
 
-            std::list<TreeNode*>::iterator randomElement(std::list<TreeNode*> *x);
-            void expandNode(TreeNode *node);
-            int genRandomTree(TreeNode *node,int node_limit);
-            void walkTree(TreeNode *node);
-            bool allocNode(TreeNode *node,unsigned int l,unsigned int r);
-            BasicBlock *createRandomBasicBlock(TreeNode *node,Function *f,Value *var,std::vector<BasicBlock*>::iterator &iter,std::map<BasicBlock*,TreeNode*> *bb_info);
-            bool spawnRandomIf(BasicBlock *from,std::vector<BasicBlock*> *son,Value *var,std::map<BasicBlock*,TreeNode*> *bb_info);
             std::vector<BasicBlock*> *getBlocks(Function *function,std::vector<BasicBlock*> *lists);
             void getAnalysisUsage(AnalysisUsage &AU);
+            Function *buildUpdateKeyFunc(Module *m);
             unsigned int getUniqueNumber(std::vector<unsigned int> *rand_list);
-            static bool valueEscapes(Instruction *Inst);
-            void DoFlatten(Function *f,int seed,int enderNum);
-            bool runOnFunction(Function &function);
+            void DoFlatten(Function *f,int seed,Function *updateFunc);
+            bool runOnModule(Module &module);
 
     };
-    FunctionPass* createFlatteningEnhancedPass(bool enable);
+    ModulePass* createFlatteningEnhancedPass(bool enable);
 }
 
 #endif // LLVM_FLATTENING_H
