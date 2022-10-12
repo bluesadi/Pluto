fa198df8e6328ff69244a9770c2cf6054be43875
GarfieldHan <2652609017@qq.com>
2022-10-12 10:07:18 +0800

    build_scripts: Don't install to system directly
    Otherwise it would influence our system's clang. Install to 'install' dir instead.
    
    Signed-off-by: GarfieldHan <2652609017@qq.com>

--- build_macos.sh
+++ build_macos.sh
@@ -5,6 +5,8 @@ cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS="clang" \
     -DDEFAULT_SYSROOT=$(xcrun --show-sdk-path) \
     -DCMAKE_OSX_SYSROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX$(xcrun --show-sdk-version).sdk \
     -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+    -DCMAKE_INSTALL_PREFIX="../install" \
     ../llvm
+
 ninja
 ninja install
