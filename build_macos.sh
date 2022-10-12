5e5dbb9e9ca74f914a120cc0485b8ee9e663cbaa
GarfieldHan <2652609017@qq.com>
2022-10-12 10:03:04 +0800

    build_macos: Let xcrun decide macos sdk version
    Signed-off-by: GarfieldHan <2652609017@qq.com>

--- build_macos.sh
+++ build_macos.sh
@@ -3,7 +3,7 @@ cd build
 cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS="clang" \
     -DCMAKE_BUILD_TYPE=Release \
     -DDEFAULT_SYSROOT=$(xcrun --show-sdk-path) \
-    -DCMAKE_OSX_SYSROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX11.3.sdk \
+    -DCMAKE_OSX_SYSROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX$(xcrun --show-sdk-version).sdk \
     -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
     ../llvm
 ninja
