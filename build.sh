b5ace25190c3a365637533903740ffa3fbb4816d
GarfieldHan <2652609017@qq.com>
2022-10-16 09:31:38 +0800

    build_scripts: Let build machine decide threads
    Signed-off-by: GarfieldHan <2652609017@qq.com>

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS="clang" \
     -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86" \
     -DBUILD_SHARED_LIBS=On -DCMAKE_INSTALL_PREFIX="../install" ../llvm
 
-ninja -j 8
+ninja -j$(nproc)
 ninja install
