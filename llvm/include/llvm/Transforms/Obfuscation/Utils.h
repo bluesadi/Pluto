bluesadi/Pluto

commit f6862fac7ad25a0415a4885da97c0994b51e5b3f

filter CXX exception rcf

llvm/include/llvm/Transforms/Utils.h
@@ -161,6 +161,12 @@ FunctionPass *createAssumeSimplifyPass();
 // don't block SCEV.
 //
 Pass *createCanonicalizeFreezeInLoopsPass();
+
+//===----------------------------------------------------------------------===//
+//
+// Check whether the instruction is a CXX Exceptions Instruction
+//
+bool is_exceptional_instruction(Instruction &I)
 } // namespace llvm
 
 #endif

llvm/lib/Transforms/Obfuscation/RandomControlFlow.cpp
@@ -21,12 +21,23 @@ bool RandomControlFlow::runOnFunction(Function &F){
         INIT_CONTEXT(F);
         SKIP_IF_SHOULD(F);
         for(int i = 0;i < ObfuTimes;i ++){
+
             vector<BasicBlock*> origBB;
-            for(BasicBlock &BB : F){
+            for(BasicBlock &BB : F)
                 origBB.push_back(&BB);
-            }
-            for(BasicBlock *BB : origBB){
-                randcf(BB);
+            
+            // filter C++ exception handling BBs
+            for(BasicBlock *BB : origBB) {
+                bool isExceptionBB = false;
+                for(Instruction &I : *BB) {
+                    if( is_exceptional_instruction(I) ){
+                        isExceptionBB = true;
+                        break;
+                    }
+                }
+
+                if( isExceptionBB == false )
+                    randcf(BB);
             }
         }
         return true;

llvm/lib/Transforms/Obfuscation/Utils.cpp
@@ -119,3 +119,18 @@ std::string llvm::readAnnotation(Function *F) {
     }
     return annotation;
 }
+
+bool llvm::is_exceptional_instruction(Instruction &I) {
+
+    if( isa<LandingPadInst>(I)  ||
+        isa<CatchPadInst>(I)    ||
+        isa<CatchSwitchInst>(I) ||
+        isa<CatchReturnInst>(I) ||
+        isa<CleanupPadInst>(I)  ||
+        isa<CleanupReturnInst>(I) ){
+        
+        return true;
+    }
+    
+    return false;
+}

commit 1f0df12d13fdf587a486a5f4f7377fba31be5c9c

RandomControlFlow: Fix compile issue introduced by f6862fa
Signed-off-by: GarfieldHan <2652609017@qq.com>

llvm/include/llvm/Transforms/Obfuscation/Utils.h
@@ -3,6 +3,7 @@
 
 #include "llvm/Support/CommandLine.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Instruction.h"
 
 #define INIT_CONTEXT(X) CONTEXT=&X.getContext()
 #define TYPE_I64 Type::getInt64Ty(*CONTEXT)
@@ -34,6 +35,9 @@ namespace llvm{
     void fixStack(Function &F);
     BasicBlock* createCloneBasicBlock(BasicBlock *BB);
     std::string readAnnotation(Function *f);
+
+    // Check whether the instruction is a CXX Exceptions Instruction
+    bool is_exceptional_instruction(llvm::Instruction &I);
 }
 
 #endif // LLVM_UTILS_H

llvm/include/llvm/Transforms/Utils.h
@@ -161,12 +161,6 @@ FunctionPass *createAssumeSimplifyPass();
 // don't block SCEV.
 //
 Pass *createCanonicalizeFreezeInLoopsPass();
-
-//===----------------------------------------------------------------------===//
-//
-// Check whether the instruction is a CXX Exceptions Instruction
-//
-bool is_exceptional_instruction(Instruction &I)
 } // namespace llvm
 
 #endif