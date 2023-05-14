682b4eb13aff3633b16f8752abb3d9b2514d12ce
bluesadi <yiboliume@gmail.com>
2023-05-14 18:38:31 +0800

    Delete install directories

--- llvm-12.0.1/install/include/llvm/Support/VCSRevision.h
+++
@@ -1,2 +0,0 @@
-#define LLVM_REVISION "3c332857429162015aef94d875eafc95a60112f8"
-#define LLVM_REPOSITORY "git@github.com:bluesadi/Pluto-Obfuscator.git"
--- llvm-12.0.1/install/include/llvm/Transforms/Obfuscation/Utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "llvm/IR/Function.h"
-
-using namespace llvm;
-
-namespace Utils {
-void fixStack(Function &F);
-}
