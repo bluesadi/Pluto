85addd718c19e0af0fa2b4cd0074daed6baf06ac
bluesadi <yiboliume@gmail.com>
2023-12-20 05:17:16 +0000

    Clear this branch

--- clang/test/Driver/apple-arm64-arch.c
+++
@@ -1,7 +0,0 @@
-// RUN: env SDKROOT="/" %clang -arch arm64 -c -### %s 2>&1 | \
-// RUN:   FileCheck %s
-//
-// REQUIRES: darwin
-// XFAIL: apple-silicon-mac
-//
-// CHECK: "-triple" "arm64-apple-ios{{[0-9.]+}}"
--- test/aes/annotation.h
+++
@@ -1,2 +0,0 @@
-#define FUNC_INCLUDE __attribute__((annotate("include")))
-#define FUNC_EXCLUDE __attribute__((annotate("exclude")))
