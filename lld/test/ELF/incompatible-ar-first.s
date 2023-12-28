b383347ff48518f191973a538494083c063a8f03
bluesadi <yiboliume@gmail.com>
2023-12-28 05:02:40 +0000

    Enable lld

---
+++ lld/test/ELF/incompatible-ar-first.s
@@ -0,0 +1,15 @@
+// REQUIRES: x86
+// RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %p/Inputs/archive.s -o %ta.o
+// RUN: rm -f %t.a
+// RUN: llvm-ar rc %t.a %ta.o
+// RUN: llvm-mc -filetype=obj -triple=i686-linux %s -o %tb.o
+// RUN: not ld.lld %t.a %tb.o -o /dev/null 2>&1 | FileCheck %s
+
+// We used to crash when
+// * The first object seen by the symbol table is from an archive.
+// * -m was not used.
+// CHECK: .a({{.*}}a.o) is incompatible with {{.*}}b.o
+
+// RUN: not ld.lld --start-lib %ta.o --end-lib %tb.o -o /dev/null 2>&1 | FileCheck %s --check-prefix=CHECK2
+
+// CHECK2: {{.*}}b.o is incompatible{{$}}
