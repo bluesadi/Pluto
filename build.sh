43a162edd9aa30c9581b4625104204cb527a08d0
bluesadi <yiboliume@gmail.com>
2023-12-28 16:56:23 +0000

    Fix building problem

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 mkdir -p build
-cmake -G Ninja -S . -B build \
+cmake -G Ninja -S llvm -B build \
+      -DLLVM_ENABLE_PROJECTS="clang;lld" \
+      -DLLVM_TARGETS_TO_BUILD="X86" \
+      -DLLVM_ENABLE_LLD=ON \
+      -DLLVM_INCLUDE_BENCHMARKS=OFF \
       -DCMAKE_C_COMPILER="gcc" \
       -DCMAKE_CXX_COMPILER="g++" \
       -DCMAKE_INSTALL_PREFIX="install" \
       -DCMAKE_BUILD_TYPE=Release
-ninja -j`nproc` -C build install
+ninja -j`nproc` -C build
