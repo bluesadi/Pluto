82c9edfb28aaee77c4e605c96cffcb387c9ff629
bluesadi <yiboliume@gmail.com>
2024-06-23 23:40:40 +0000

    Update

--- examples/test.sh
+++ examples/test.sh
@@ -1,2 +1,5 @@
 ../install/bin/clang -O0 example.c -o example
 ../install/bin/clang -O0 -mllvm -passes=bcf example.c -o example.obfu
+
+../install/bin/clang -O0 hello.c -o hello
+../install/bin/clang -O0 -mllvm -passes=bcf hello.c -o hello.obfu
